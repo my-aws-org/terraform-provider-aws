eb7902dc556adfc2130db9c39581b90d715be8e1
[USERNAME] <san@vmtex.sg>
2020-09-26 21:08:15 +0800

    Change to mumbai

--- examples/two-tier/variables.tf
+++ examples/two-tier/variables.tf
@@ -14,7 +14,8 @@ variable "key_name" {
 
 variable "aws_region" {
   description = "AWS region to launch servers."
-  default     = "us-east-1"
+#  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 # Ubuntu Precise 12.04 LTS (x64)
@@ -23,6 +24,7 @@ variable "aws_amis" {
     eu-west-1 = "ami-674cbc1e"
 #    us-east-1 = "ami-1d4e7a66"
     us-east-1 = "ami-00c7c1cf5bdc913ed"
+    ap-south-1 = "ami-0c0dc7872b44903a6"
     us-west-1 = "ami-969ab1f6"
     us-west-2 = "ami-8803e0f0"
   }
